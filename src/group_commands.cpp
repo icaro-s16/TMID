79bf583b7cfe843b846a5d80a688d58c8f0209f2
DharioNewbery <dharionew@gmail.com>
2026-02-28 10:04:04 -0300

    minor changes

--- src/group_commands.cpp
+++ src/group_commands.cpp
@@ -1,41 +1,39 @@
 #include "group_commands.hpp"
 
-namespace group {
     
-    void create_group_config_file()
-    {
-        if (std::filesystem::exists(GROUP_CONFIG_FILE_NAME)) {
-            std::cout << GROUP_CONFIG_FILE_NAME << " exists. Aborting file creation" <<std::endl;
-            return;
-        }
-
-        std::ofstream config_file(GROUP_CONFIG_FILE_NAME);
-        if (config_file.is_open()) {
-            std::cout << "file created." << std::endl;
-        } else {
-            std::cerr << "failed to create file." << std::endl;
-            throw;
-        }
-
-        set_config_file(config_file);
+void group::create_group_config_file()
+{
+    if (std::filesystem::exists(GROUP_CONFIG_FILE_NAME)) {
+        std::cout << GROUP_CONFIG_FILE_NAME << " exists. Aborting file creation" <<std::endl;
+        return;
     }
 
-    void set_config_file(std::ofstream& config_file)
-    {
-        config_file << "# group's config file.\n";
-        config_file << "# lines starting with '#' will be ignored by the system.\n";
-        config_file << "\n";
-        config_file << "# group name: usually the folder name\n";
-        config_file << "group_name=\n";
-        config_file << "\n";
-        config_file << "# participants: all the participants for the group, with their unique id\n";
-        config_file << "participants=\n";
-
-        config_file.close();
+    std::ofstream config_file(GROUP_CONFIG_FILE_NAME);
+    if (config_file.is_open()) {
+        std::cout << "file created." << std::endl;
+    } else {
+        std::cerr << "failed to create file." << std::endl;
+        throw;
     }
 
-    std::unique_ptr<Group> read_group_config()
-    {
-        return std::unique_ptr<Group>();
-    }
+    set_config_file(config_file);
+}
+
+void group::set_config_file(std::ofstream& config_file)
+{
+    config_file << "# group's config file.\n";
+    config_file << "# lines starting with '#' will be ignored by the system.\n";
+    config_file << "\n";
+    config_file << "# group name: usually the folder name\n";
+    config_file << "group_name=\n";
+    config_file << "\n";
+    config_file << "# participants: all the participants for the group, with their unique id\n";
+    config_file << "participants=\n";
+
+    config_file.close();
+}
+
+std::unique_ptr<Group> group::read_group_config()
+{
+    return std::unique_ptr<Group>();
 }
