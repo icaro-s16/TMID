12cb050aea14ea30b2e90e0e830d3a3448734a61
icaro-s16 <santanaikaru@gmail.com>
2026-03-07 13:35:01 -0300

    create ipRegex function

---
+++ src/regex.cpp
@@ -0,0 +1,40 @@
+#ifndef REGEX_HPP
+#define REGEX_HPP
+
+#include <string>
+#include <regex>
+#include <iostream>
+
+enum IP{IPv4, IPv6, UNDEF};
+
+int ipRegex(std::string ip){
+    std::regex ipv4_reg(R"(^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$)");
+    std::regex ipv6_reg(R"(^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$)");
+    std::smatch match;
+    if(std::regex_match(ip, match, ipv4_reg)) return IPv4;
+    if(std::regex_match(ip, match, ipv6_reg)) return IPv6;
+    return UNDEF;
+}
+
+
+int main(){
+    std::string ip;
+    std::cin >> ip;
+    int ret = ipRegex(ip);
+    switch(ret){
+        case IPv4:
+            std::cout << "IPv4" << std::endl;
+            break;
+        case IPv6:
+            std::cout << "IPv6" << std::endl;
+            break;
+        default:
+            std::cout << "UNDEF" << std::endl;
+            break;
+    }
+    return 0;
+
+}
+
+
+#endif
